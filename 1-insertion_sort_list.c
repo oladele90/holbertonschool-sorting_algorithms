2e2fdadd633f5146973b9ab6c3fc7185b68fb040
oladele90 <oladelemiro07@gmail.com>
2022-12-19 17:15:49 -0800

    insertion

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+void swap2n(listint_t **h, listint_t **a, listint_t *b)
+{
+	(*a)->next = b->next;
+	if (b->next != NULL)
+		b->next->prev = *a;
+	b->prev = (*a)->prev;
+	b->next = *a;
+	if ((*a)->prev != NULL)
+		(*a)->prev->next = b;
+	else
+		*h = b;
+	(*a)->prev = b;
+	*a = b->prev;
+}
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *a, *b, *temp;
+
+	if(*list == NULL || (*list)->next == NULL)
+		return;
+	for (b = (*list)->next; b != NULL; b = temp)
+	{
+		temp = b->next;
+		a = b->prev;
+		while (a != NULL && b->n < a->n)
+		{
+			swap2n(list, &a, b);
+			print_list((const listint_t *)*list);
+		}
+	}
+}
