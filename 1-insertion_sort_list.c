36eb478b576dcc5ffaf44b35aa0e46308b5d07ec
oladele90 <oladelemiro07@gmail.com>
2022-12-19 17:22:56 -0800

    fix betty

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,4 +1,10 @@
 #include "sort.h"
+/**
+ * swap2n - swaps two nodes
+ * @h: pointer to head
+ * @a: node to swap
+ * @b: node to swap
+ */
 
 void swap2n(listint_t **h, listint_t **a, listint_t *b)
 {
@@ -15,11 +21,20 @@ void swap2n(listint_t **h, listint_t **a, listint_t *b)
 	*a = b->prev;
 }
 
+/**
+ * insertion_sort_list -  sorts a doubly linked list using the Insertion
+ * sort algorithm
+ * @list : pointer to list
+ */
+
+
+
+
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *a, *b, *temp;
 
-	if(*list == NULL || (*list)->next == NULL)
+	if (*list == NULL || (*list)->next == NULL || list == NULL)
 		return;
 	for (b = (*list)->next; b != NULL; b = temp)
 	{
