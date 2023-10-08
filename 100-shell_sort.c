e81777429345fbb5f65b54fab25e9f9c1809394e
oladele90 <oladelemiro07@gmail.com>
2023-10-08 22:22:39 +0000

    shell sort

---
+++ 100-shell_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+
+/**
+* shell_sort - sorts an array of int in ascending order using the Knuth seq
+* @array: array of ints to sort
+* @size: size of array
+* Return: none
+*/
+void shell_sort(int *array, size_t size)
+{
+	int gap = 1, i, hold, j = 0;
+
+	while ((size_t)gap <= size / 3)
+		gap = gap * 3 + 1;
+	for (; gap >= 1; gap = (gap - 1) / 3)
+	{
+		for (i = gap; i < (int)size; i++)
+		{
+			hold = array[i];
+			for (j = i - gap; array[j] > hold && j >= 0; j = j - gap)
+				array[j + gap] = array[j];
+			array[j + gap] = hold;
+		}
+		for (j = 0; j < (int)size - 1; j++)
+			printf("%d, ", array[j]);
+		printf("%d\n", array[j]);
+	}
+}
